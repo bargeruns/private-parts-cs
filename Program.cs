196cb4c65af62ce86273383a037fa0530cc4f967
Sean <sean@layer3tv.com>
2017-06-07 11:29:14 -0600

    initial commit with readme

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace private_parts_cs
+{
+    class Program
+    {
+        public class Person
+        {
+            public Person(string firstName, string lastName, DateTime dateOfBirth)
+            {
+                this.FirstName = firstName;
+                this.LastName = lastName;
+                this.DateOfBirth = dateOfBirth;
+            }
+
+            protected string FirstName { get; private set; }
+            protected string LastName { get; private set; }
+            public DateTime DateOfBirth { get; private set; }
+
+            public string FullName { get { return $"{this.FirstName} {this.LastName}"; } }
+
+            public bool IsAdult()
+            {
+                return this.DateOfBirth > DateTime.Today.AddYears(-18);
+            }
+
+        }
+
+        public class Student : Person
+        {
+            public Student(string firstName, string lastName, DateTime dateOfBirth)
+                : base(firstName, lastName, dateOfBirth)
+            { }
+
+            public string SchoolName { get; set; }
+            
+            public string RosterName { get { return $"{this.LastName}, {this.FirstName}"; } }
+        }
+
+        public class Course
+        {
+            public Course(List<Student> enrolledStudents, string name)
+            {
+                this.EnrolledStudents = enrolledStudents;
+                this.Name = name;
+            }
+
+            public string Name { get; private set; }
+
+            protected List<Student> EnrolledStudents { get; private set; }
+
+            public string CourseName()
+            {
+                return this.Name;
+            }
+
+            public void PrintRoster() {
+                foreach(Student student in EnrolledStudents) {
+                    Console.WriteLine(student.RosterName);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Student John = new Student("John", "Doe", new DateTime(1992,04,24));
+            Student Jane = new Student("Jane", "Says", new DateTime(1998,05,08));
+
+            John.SchoolName = "Westview";
+            Jane.SchoolName = "Chesterton";
+
+            Course Eng101 = new Course(new List<Student> {John, Jane}, "Eng101");
+
+            Eng101.PrintRoster();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Private Parts CS
+
+A simple C# console application demonstrating the basics of scope and access modifiers. Play around with the `Course` and `Student` classes!
+
+See if you can create a few Students and a Course inside of `Main ()`. Then, see if you can access any of the private or protected information in each of the Students via the Course (i.e. `EnglishCourse.EnrolledStudents[0].FirstName`).
